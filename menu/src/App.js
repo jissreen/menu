77dbeced1044932493c3fd314e9e5cc7dc18f345
jisha <jishsreenth2016@gmail.com>
2023-04-04 06:50:12 +0400

    first commit

---
+++ menu/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+function App() {
+  return (
+    <div >
+      <Router>
+        
+      <Navbar/>
+      </Router>
+    </div>
+  )
+}
+
+export default App
