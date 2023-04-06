4f29739a27c7a343ed03c806e117f5612b056014
jisha <jishsreenth2016@gmail.com>
2023-04-06 06:41:13 +0400

    features added

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 function App() {
   return (
-    <div >
+    <div className="App">
       <Router>
         
       <Navbar/>
+      <Routes>
+        <Route path="/" exact element={<Home/>}/>
+      </Routes>
       </Router>
     </div>
   )
--- menu/src/Components/Navbar.js
+++ menu/src/Components/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import  "../styles/Navbar.css"
 import Logo from '../assets/pizzaLogo.png'
+import {Link} from 'react-router-dom'
+
 
 function Navbar() {
   return (
-    <div className="Navbar">
+    <div className="navbar">
         <div className="leftSide">
             <img src={Logo}/>
         </div>
         <div className="rightSide">
+          <Link to='/'>Home </Link>
+          <Link to='/menu'>menu </Link>
+          <Link to='/about'>about </Link>
+          <Link to='/contact'>contact </Link>
 
         </div>
       
