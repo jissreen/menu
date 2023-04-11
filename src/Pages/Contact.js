b70825ab4a5ccead046d9edb0b41ca6803d70614
jisha <jishsreenth2016@gmail.com>
2023-04-11 15:15:43 +0400

    contact page modified

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PizzaLeft from '../assets/pizzaLeft.jpg'
+import '../styles/Contact.css'
 
 function Contact() {
   return (
@@ -10,6 +11,22 @@ function Contact() {
         </div>
 
         <div className="rightSide">
+          <h1>Contact Us</h1>
+          <form id="contact-form" method="POST">
+            <label htmlFor="name">Full Name</label>
+            <input name="name" placeholder="Enter full name..." type="text" />
+            <label htmlFor="email">Email</label>
+            <input name="email" placeholder="Enter email..." type="email" />
+            <label htmlFor="message">Message</label>
+            <textarea rows="6"
+            placeholder="Enter Message ..."
+            name="message"
+            required></textarea>
+            <button type="submit">Send Message</button>
+
+
+
+          </form>
            
         </div>
 
